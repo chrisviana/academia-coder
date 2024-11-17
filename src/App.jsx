1425d924be990e23c28a82968ffee09459ba89a2
Christian Viana <viana2015christian@gmail.com>
2024-11-17 19:08:54 -0300

    Aula 01 - Inciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+
+  const x = "Teste"
+  console.log(x)
+  return (
+    <div>
+      <h1>Hello Turma!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci impedit ipsa totam culpa vero voluptatum voluptatem vel doloribus ratione temporibus, laudantium dolor obcaecati aliquid cupiditate libero officia. Impedit, dolores expedita.
+
+      </p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+      <form>
+        <input type="text" name="name" />
+      </form>
+    </div>
+   
+  )
+}
+
+export default App
